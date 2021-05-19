d1b8dbf6dd06160726e5d6f1924fc049ec702592
Leandro Ramos de Oliveira <leoramos680@gmail.com>
2021-05-18 21:40:01 -0300

    adicionando buefy;bulma

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
+    <router-link to="/">Home</router-link>
     <router-link to="/about">About</router-link>
   </div>
   <router-view />
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+@charset "utf-8";
+</style>
